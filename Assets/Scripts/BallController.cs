70b4046f01ccde103e1f164f3d3998e1a03f7c5e
shadowman9443 <shadowman9443@gmail.com>
2017-12-13 15:46:03 +0600

    First commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float moveHorizonatl = Input.GetAxis ("Horizontal");
+
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizonatl, 0.0f, moveVertical);
+		rb.AddForce (movement);
+
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+
+	}
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 5)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
